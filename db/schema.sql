ef841b8caeaf5b1ea3406af29b2267a10048d99e
Foos Mahamud <foosmahamud55@gmail.com>
2020-10-12 22:52:28 -0500

    created the initial database

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+    CREATE DATABASE burger_db;
+    USE burger_db;
+    CREATE TABLE burgers (
+        id int NOT NULL AUTO_INCREMENT,
+        name varchar(244) NOT NULL,
+       devoured BOOLEAN DEFAULT false,
+        PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ("Cheese burger", false);
+INSERT INTO burgers (name, devoured) VALUES ("chicken sandwich", false);
+INSERT INTO burgers (name, devoured) VALUES ("ww burger", false);
