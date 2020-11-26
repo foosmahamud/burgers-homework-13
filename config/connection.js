b8c686a1a31f79a69701807b9f744048dfa2fc25
Foos Mahamud <foosmahamud55@gmail.com>
2020-11-25 18:50:35 -0600

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Hooyo@4730",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // Replace user and password with your MySQL login info
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
